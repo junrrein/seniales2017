4b38e4039a8de346a8dd0864ab1e17774f6173d4
Julián Unrrein <junrrein@gmail.com>
2017-03-28 20:44:49 -0300

    Remover función para cambiar tipo y tamaño de fuente

--- setFont.m
+++
@@ -1,5 +0,0 @@
-function setFont (fontName, fontSize)
-    fig = gcf;
-    set(findall(fig, '-property', 'FontName'), 'FontName', fontName);
-    set(findall(fig, '-property', 'FontSize'), 'FontSize', fontSize);
-endfunction
