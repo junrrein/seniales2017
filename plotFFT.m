fdb9cb2f8c0b854479adcc9839e2aa3790523105
Julián Unrrein <junrrein@gmail.com>
2017-04-07 14:42:16 -0300

    Agregar etiquetas al gráfico de plotFFT

--- plotFFT.m
+++ plotFFT.m
@@ -2,3 +2,7 @@ function plotFFT (senial, fm)
 
 deltaF = fm / length(senial);
 plot(getFFTBounds(senial) * deltaF, fftshift(abs(fft(senial))));
+xlabel("Frecuencia (Hz)")
+ylabel("Magnitud")
+grid on
+
