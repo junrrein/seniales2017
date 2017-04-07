eb23cfaf8b4172ce5e4dccd06403a5a401111950
matyaspr <matyaspr@gmail.com>
2017-04-07 19:01:57 -0300

    borrar funciones de ploteo de julian

--- getFFTBounds.m
+++
@@ -1,9 +0,0 @@
-function out = getFFTBounds(signal)
-   N = length(signal);
-   
-   if (mod(N, 2) == 0)
-      out = -N/2 : (N/2 - 1);
-   else
-      out = -(N-1)/2 : (N-1)/2;
-   endif
-endfunction
--- plotFFT.m
+++
@@ -1,8 +0,0 @@
-function plotFFT (senial, fm)
-
-deltaF = fm / length(senial);
-plot(getFFTBounds(senial) * deltaF, fftshift(abs(fft(senial))));
-xlabel("Frecuencia (Hz)")
-ylabel("Magnitud")
-grid on
-
