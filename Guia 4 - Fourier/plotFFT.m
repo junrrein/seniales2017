408bb1b43897c689f008a0ff3dbfe76f3435039b
Julián Unrrein <junrrein@gmail.com>
2017-04-10 14:53:25 -0300

    TP4: Corregir error en plotFFT en que se utilizaba la frecuencia de muestreo en lugar de la cantidad de muestras

--- Guia 4 - Fourier/plotFFT.m
+++ Guia 4 - Fourier/plotFFT.m
@@ -3,10 +3,10 @@ function plotFFT (senial, fm)
 N = length(senial);
 espectroMagnitud = abs(fft(senial));
 
-dominioFrecuencial = -fm/2 + 1 : fm/2;
+dominioFrecuencial = -N/2 + 1 : N/2;
 dominioFrecuencial *= fm/N; 
 
-espectroMagnitud = shift(espectroMagnitud, fm/2 - 1);
+espectroMagnitud = shift(espectroMagnitud, N/2 - 1);
 espectroMagnitud /= N;
 
 stem(dominioFrecuencial, espectroMagnitud)
