c1282d0d2e206b36fc7218edb2ed4ad218d1429f
matyaspr <matyaspr@gmail.com>
2017-04-13 16:52:37 -0300

    TP2/sistemaLineal: Documentar función y cambiar variables para reflejar lo dado en el libro

--- Guia 2/sistemaLineal.m
+++ Guia 2/sistemaLineal.m
@@ -1,23 +1,34 @@
 function salida = sistemaLineal (A, B, entrada)
-% te la debo
+% Entradas:
+% A : Coeficientes a_k del sistema lineal
+% B : Coeficientes b_k del sistema lineal
+% entrada : entrada del sistema
+% Se asume que el sistema es causal.
+% ======================================
+% Salidas:
+% salida: Salida del sistema a la entrada dada
 
+% Se inicializa el vector de salida
 salida = zeros(1, length(entrada) + length(B) - 1);
 
 % Evaluar la salida para cada instante de tiempo
-for ii = 1 : length(salida);
+M = length(B);
+N = length(A);
+
+for n = 1 : length(salida);
     % Iteramos sobre los coeficientes b
-    for jj = 1 : length(B)
-        if (ii - jj + 1 > 0 && ii - jj + 1 <= length(entrada))
+    for kk = 1 : M
+        if (n - kk + 1 > 0 && n - kk + 1 <= length(entrada))
             % Notar que se divide por a0
-            salida(ii) += B(jj) * entrada(ii - jj + 1) / A(1);
+            salida(n) += B(kk) * entrada(n - kk + 1) / A(1);
         end
     end
     
     % Iteramos sobre los coeficientes a
-    for kk = 2 : length(A)
-        if (ii - kk + 1 > 0)
+    for kk = 2 : N
+        if (n - kk + 1 > 0)
             % Notar que se divide por a0
-            salida(ii) += A(kk) * salida(ii - kk + 1) / A(1);
+            salida(n) += A(kk) * salida(n - kk + 1) / A(1);
         end
     end
 end
