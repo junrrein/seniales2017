73dcc67314344bf67b07cb44ff2c8b00a66e58c9
matyaspr <matyaspr@gmail.com>
2017-06-10 19:48:57 -0300

    TP Final/RegresionLineal: Evitar divisiones por cero

--- TP Final/regresionLineal.m
+++ TP Final/regresionLineal.m
@@ -5,4 +5,9 @@ X = [ones(length(x),1) x];
 B = X \ y;
 
 yRegresion = X * B;
-rCuadrado = 1 - sum((y - yRegresion).^2)/sum((y - mean(y)).^2);
+
+if (sum((y - mean(y)).^2) < 1e-7)
+    rCuadrado = 0;
+else
+    rCuadrado = 1 - sum((y - yRegresion).^2)/sum((y - mean(y)).^2);
+end
